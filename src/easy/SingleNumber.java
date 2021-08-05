e5c8912d076f33388c3dc33ac308e354486e1344
Anvar Kalykov <kalykov.anvar@gmail.com>
2021-08-06 00:21:38 +0600

    05.08.2021

--- src/easy/SingleNumber.java
+++ src/easy/SingleNumber.java
@@ -2,7 +2,7 @@ package easy;
 
 public class SingleNumber {
     public int singleNumber(int[] nums) {
-        int s =0;
+        int s = 0;
         for (int num : nums) {
             s = s ^ num;
         }
