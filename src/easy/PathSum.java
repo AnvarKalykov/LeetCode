5bb25e5b43f54a2f53b4ecffdccd07152b6f4478
Anvar Kalykov <kalykov.anvar@gmail.com>
2021-08-17 23:35:23 +0600

    17.08.2021

--- src/easy/PathSum.java
+++ src/easy/PathSum.java
@@ -17,13 +17,10 @@ class Solution112 {
     public boolean hasPathSum(PathSum root, int targetSum) {
         if (root == null) {
             return false;
+        } else if (root.left == null && root.right == null && targetSum - root.val == 0) {
+            return true;
+        } else {
+            return hasPathSum(root.left,targetSum - root.val)||hasPathSum(root.right,targetSum - root.val);
         }
-        if (root.left == null && root.right == null) {
-            if (targetSum == root.val) {
-                return true;
-            } else {
-                return false;
-            }
-        } return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
     }
 }
