fc0f35ec68be31555433a4f109e776b98ca2a1f3
Anvar Kalykov <kalykov.anvar@gmail.com>
2021-08-30 22:24:14 +0600

    30.08.2021

--- src/medium/FindAllDuplicatesInArray.java
+++ src/medium/FindAllDuplicatesInArray.java
@@ -6,9 +6,8 @@ import java.util.List;
 public class FindAllDuplicatesInArray {
     public List<Integer> findDuplicates(int[] nums) {
         List<Integer> answer = new LinkedList<>();
-        for(int i = 0; i<nums.length; i++)
-        {
-            int j = Math.abs(nums[i])-1;
+        for(int i = 0; i<nums.length; i++) {
+            int j = Math.abs(nums[i]) - 1;
             if(nums[j] >= 0)
                 nums[j] *= -1;
             else
